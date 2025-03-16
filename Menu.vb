bd324f9e8e0aab9c067aaeb9517647fb0449b15b
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-03-15 20:40:14 -0400

    Menu

--- Menu.vb
+++ Menu.vb
@@ -2,25 +2,67 @@
     Inherits Form
 
     Public Sub New()
-        Me.Text = "Pantalla Principal"
-        Me.Size = New Size(800, 600)
+        ' Configuración del formulario
+        Me.Text = "Menú"
+        Me.Size = New Size(400, 700)
         Me.BackColor = Color.White
 
-        Dim lblBienvenida As New Label With {
-            .Text = "¡Bienvenido al Sistema!",
-            .Font = New Font("Arial", 24, FontStyle.Bold),
-            .AutoSize = True,
-            .Location = New Point(250, 250)
+        ' Panel superior con título
+        Dim panelSuperior As New Panel With {
+            .Size = New Size(Me.Width, 60),
+            .BackColor = Color.Navy,
+            .Dock = DockStyle.Top
         }
 
-        Me.Controls.Add(lblBienvenida)
+        Dim lblMenu As New Label With {
+            .Text = "Menú",
+            .ForeColor = Color.White,
+            .Font = New Font("Arial", 16, FontStyle.Bold),
+            .AutoSize = True,
+            .Location = New Point(20, 15)
+        }
+
+        panelSuperior.Controls.Add(lblMenu)
+        Me.Controls.Add(panelSuperior)
+
+        ' Crear las opciones del menú sin imágenes
+        CrearOpcionMenu("Facultades", 100)
+        CrearOpcionMenu("Sugerencias", 200)
+        CrearOpcionMenu("Maestros", 300)
+
+        ' Botón de salida
+        Dim btnSalir As New Button With {
+            .Text = "Salir",
+            .Font = New Font("Arial", 12, FontStyle.Bold),
+            .Size = New Size(100, 40),
+            .Location = New Point(20, Me.Height - 100)
+        }
+        AddHandler btnSalir.Click, AddressOf Me.CerrarAplicacion
+        Me.Controls.Add(btnSalir)
     End Sub
 
-    Private Sub Menu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub CrearOpcionMenu(texto As String, yPos As Integer)
+        Dim panelOpcion As New Panel With {
+            .Size = New Size(Me.Width - 40, 80),
+            .Location = New Point(20, yPos),
+            .BackColor = Color.LightGray,
+            .BorderStyle = BorderStyle.FixedSingle
+        }
 
+        Dim lblTexto As New Label With {
+            .Text = texto,
+            .Font = New Font("Arial", 14, FontStyle.Bold),
+            .ForeColor = Color.Black,
+            .AutoSize = False,
+            .TextAlign = ContentAlignment.MiddleCenter,
+            .Dock = DockStyle.Fill
+        }
+
+        panelOpcion.Controls.Add(lblTexto)
+        Me.Controls.Add(panelOpcion)
     End Sub
 
-
+    Private Sub CerrarAplicacion(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
 End Class
-
-
